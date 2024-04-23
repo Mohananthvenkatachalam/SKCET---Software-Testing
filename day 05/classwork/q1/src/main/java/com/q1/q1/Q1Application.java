63f9dc90d8f6df8777db9d1450c185b54ae2ba2f
Mohananthvenkatachalam <mohananthvs18@gmail.com>
2024-04-23 14:55:45 +0530

    adding channges in day 5 home work

--- day 05/classwork/q1/src/main/java/com/q1/q1/Q1Application.java
+++ day 05/classwork/q1/src/main/java/com/q1/q1/Q1Application.java
@@ -1,11 +1,43 @@
 package com.q1.q1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Q1Application {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+
+		// geting to the demoblaze website
+		driver.get("https://www.demoblaze.com/");
+
+		// clicking the laptop items
+		driver.findElement(By.xpath("//*[@id=\"itemc\"]")).click();
+
+		Thread.sleep(30000);
+		// click the mackbook air
+		driver.findElement(By.xpath("//*[@id=\"tbodyid\"]/div[3]/div")).click();
+
+		// give add to cart
+		driver.findElement(By.linkText("Add to cart")).click();
+
+		// click the ok buttorn in the alert
+		driver.switchTo().alert().accept();
+		driver.navigate().back();
+
+		// go to cart
+		driver.findElement(By.xpath("//*[@id=\"navbarExample\"]/ul/li[4]")).click();
+
+		// get text
+		System.out.print(driver.findElement(By.xpath("//*[@id=\"tbodyid\"]/tr[1]/td[2]")).getText());
+
+		// close the driver
+		driver.close();
+
 		SpringApplication.run(Q1Application.class, args);
 	}
 }
--- day 05/homework/q1/src/main/java/com/q1/q1/Q1Application.java
+++ day 05/homework/q1/src/main/java/com/q1/q1/Q1Application.java
@@ -1,12 +1,50 @@
 package com.q1.q1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Q1Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+
+		// get the page
+		driver.get("https://magento.softwaretestingboard.com/");
+
+		// search shores
+		driver.findElement(By.id("search")).sendKeys("shoes");
+		driver.findElement(By.xpath("//*[@id=\"search_mini_form\"]/div[1]/label")).click();
+
+		// hover on the mens
+		Actions ac = new Actions(driver);
+		ac.moveToElement(driver.findElement(By.xpath("//*[@id=\"ui-id-5\"]"))).build().perform();
+
+		// selects tops
+		driver.findElement(By.xpath("//*[@id=\"narrow-by-list2\"]/dd/ol/li[1]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[4]/div[2]/div[2]/div/ul[1]/li[1]/a")).click();
+
+		// navigate back to the page
+		Thread.sleep(3000);
+		driver.navigate().to("https://magento.softwaretestingboard.com/");
+		driver.findElement(
+				By.xpath("//*[@id=\"maincontent\"]/div[3]/div/div[3]/div[3]/div/div/ol/li[1]/div/div/strong/a"))
+				.click();
+		driver.findElement(By.xpath("//*[@id=\"option-label-size-143-item-168\"]")).click();
+		driver.findElement(By.xpath("//*[@id=\"option-label-color-93-item-56\"]")).click();
+
+		driver.findElement(By.xpath("//*[@id=\"qty\"]")).sendKeys("4");
+
+		driver.findElement(By.xpath("//*[@id=\"product-addtocart-button\"]")).submit();
+
+		Thread.sleep(3000);
+		driver.close();
+
 		SpringApplication.run(Q1Application.class, args);
 	}
 
